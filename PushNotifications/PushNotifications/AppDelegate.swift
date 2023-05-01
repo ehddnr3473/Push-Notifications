c0363ca91970a2f53f3dac119d8fe6165b6a746b
Donguk Kim <ehddnr73@naver.com>
2023-05-01 23:27:50 +0900

    Push Notifications configured.

--- PushNotifications/PushNotifications/AppDelegate.swift
+++ PushNotifications/PushNotifications/AppDelegate.swift
@@ -9,12 +9,31 @@ import UIKit
 import Firebase
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         
+        Messaging.messaging().delegate = self
+        UNUserNotificationCenter.current().delegate = self
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { success, _ in
+            guard success else { return }
+            
+            print("Success in APNS registry")
+        }
+        
+        application.registerForRemoteNotifications()
+        
         return true
+    }
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        messaging.token { token, _ in
+            guard let token = token else { return }
+            
+            print("Token: \(token)")
+        }
     }
 }
 
